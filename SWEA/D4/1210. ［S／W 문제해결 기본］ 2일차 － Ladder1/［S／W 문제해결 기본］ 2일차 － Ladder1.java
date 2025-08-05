6fca61e16148dba28122e3ca886d3b25936dfd51
YooKyuBong <kyubongbong@gmail.com>
2025-08-05 15:26:30 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 301 ms, Memory: 100,220 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -14,11 +15,11 @@ class Solution
 		{
 		
          	int test = sc.nextInt();
-            // 첫번째 행에 값이 1인 부분에서만 출발 가능
+            // 마지막 행 -> 첫번째 행 
             
 			// 좌우에 사다리가 있는지 확인
-            // 1. 사다리가 있다면 좌/우 사다리로 이동 -> 밑으로 가는 사다리가 나올때 까지
-            // 2. 사다리가 없다면 밑으로 이동
+            // 1. 사다리가 있다면 좌/우 사다리로 이동 -> 위로 가는 사다리가 나올때 까지
+            // 2. 사다리가 없다면 위로 이동
             
             // 다른 막대를 가로지르는 막대가 없기 때문에 가능
             
@@ -31,52 +32,51 @@ class Solution
             }
             
             // 좌우 상하 배열
-            int[][] drc = new int[][]{{0,-1}, {0,1}, {-1, 0}};
             int[] nowIndex = new int[]{0,0};
             int ans = 0;
             
-			// 첫줄 숫자가 1인지 0인지 판단
-            for(int startCol = 0; startCol < ladder[0].length; startCol++){
-            	if(ladder[0][startCol] != 1) continue;
-                
-                // 시작 위치 (행, 열)
-                int row = 0;
-                int col = startCol;
-                
-                // num이 1이면 출발
-               	while(row < 100){
-                    // 2를 발견하면 시작 위치의 col값을 ans에 저장한 후while문, for문 break;
-                	if(ladder[row][col] == 2){
-                    	ans = startCol;
-                        break;
-                    }
-                    
-                    // 오른쪽 이동
-                    // 더이상 오른쪽으로 이동할 수 없으면 밑으로 한칸 이동
-                    if(col < ladder[row].length - 1 && ladder[row][col + 1] == 1){
-                    	while(col < ladder[row].length - 1 && ladder[row][col+1] == 1){
-                        	col++;
-                        }
-                        row++;
-                    }
-                    
-                    // 왼쪽 이동
-                    // 더이상 왼쪽으로 이동할 수 없으면 밑으로 한칸 이동
-                    else if(col > 0 && ladder[row][col - 1] == 1){
-                    	while(col > 0 && ladder[row][col - 1] == 1){
-                        	col--;
-                        }
-                        row++;
-                    }
-                    
-                    // 아래쪽 이동
-                    else{
-                    	row++;
-                    }
-                    
-                }
-             	if(ans != 0) break;
+            
+            int row = 99;
+            // 숫자 2의 인덱스 col찾기
+            int startCol = 0;
+            for(int i = 0; i < 100; i++) {
+            	if(ladder[row][i] == 2) {
+            		startCol = i;
+            		break;
+            	}
             }
+
+            int col = startCol;
+            
+			while(row > 0) {
+				
+				
+				// 현재 칸의 왼쪽에 1이 존재하는지 확인
+				// 있다면 col을 -1함.
+				if(col > 0 && ladder[row][col-1] == 1) {
+					while(col > 0 && ladder[row][col-1] == 1) {
+						col--;
+					}
+				}
+				
+				// 현재 칸의 오른쪽에 1이 존재하는지 확인
+				// 있다면 col을 +1함.
+				else if(col  < 99 && ladder[row][col+1] == 1) {
+					while(col  < 99 && ladder[row][col+1] == 1) {
+						col++;
+					}
+				}
+				
+					row--;
+				
+				
+				
+				if(row == 0 && ladder[row][col] == 1) {
+					ans = col;
+					break;
+				}
+				
+			}
             
             System.out.println("#" + test_case + " " + ans);
 		}
