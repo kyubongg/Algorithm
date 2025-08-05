433981e291fac8880b50287a03df35f6cd765144
YooKyuBong <kyubongbong@gmail.com>
2025-08-05 17:02:08 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 289 ms, Memory: 101,136 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -1,4 +1,7 @@
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -17,9 +20,10 @@ class Solution
          	int test = sc.nextInt();
             // 마지막 행 -> 첫번째 행 
             
-			// 좌우에 사다리가 있는지 확인
-            // 1. 사다리가 있다면 좌/우 사다리로 이동 -> 위로 가는 사다리가 나올때 까지
-            // 2. 사다리가 없다면 위로 이동
+			// 1. 각 행마다 사다리가 있는 구간을 체크한다.
+         	// 2. 가장 아래 행에 있는 2부터 시작해서, 위로 올라간다.
+         	// 2-1. 위의 행에 사다리가 존재하는 구간이 2가 있는 열과 연속된다면
+         	//		연속되는 값 중에 가장 끝 값으로 옮긴다.
             
             // 다른 막대를 가로지르는 막대가 없기 때문에 가능
             
@@ -29,12 +33,7 @@ class Solution
             	for(int j = 0; j < 100; j++){
                 	ladder[i][j] = sc.nextInt();
                 }
-            }
-            
-            // 좌우 상하 배열
-            int[] nowIndex = new int[]{0,0};
-            int ans = 0;
-            
+            }            
             
             int row = 99;
             // 숫자 2의 인덱스 col찾기
@@ -47,38 +46,41 @@ class Solution
             }
 
             int col = startCol;
-            
+            int ans = 0;
 			while(row > 0) {
+				row--;
 				
+
+				List<Integer> list = new ArrayList<>();
+				// 윗줄에 사다리가 존재하는 구간 체크
+				for(int i = 0; i < 100; i++) {
+					if(ladder[row][i] == 1) list.add(i);
+				}
 				
-				// 현재 칸의 왼쪽에 1이 존재하는지 확인
-				// 있다면 col을 -1함.
-				if(col > 0 && ladder[row][col-1] == 1) {
-					while(col > 0 && ladder[row][col-1] == 1) {
-						col--;
+				for(int i = 0; i < list.size(); i++) {
+					// 2가 존재하는 col과 붙어있는 사다리가 있다면
+					// 사다리의 길이만큼 이동
+					// col - list.get(i) == 1이면 왼쪽으로 이동
+					if(i > 0 && col - list.get(i) == 1) {
+						while(i > 0 && col - list.get(i) == 1) {
+							col--;
+							i--;
+						}
+						break;
+					}else if(i < list.size() && col - list.get(i) == -1) {
+						// col - list.get(i) == 1이면 오른쪽으로 이동
+						while(i < list.size() && col - list.get(i) == -1) {
+							col++;
+							i++;
+						}
+						break;
 					}
 				}
-				
-				// 현재 칸의 오른쪽에 1이 존재하는지 확인
-				// 있다면 col을 +1함.
-				else if(col  < 99 && ladder[row][col+1] == 1) {
-					while(col  < 99 && ladder[row][col+1] == 1) {
-						col++;
-					}
-				}
-				
-					row--;
-				
-				
-				
-				if(row == 0 && ladder[row][col] == 1) {
-					ans = col;
-					break;
-				}
+
 				
 			}
             
-            System.out.println("#" + test_case + " " + ans);
+            System.out.println("#" + test_case + " " + col);
 		}
 	}
 }
