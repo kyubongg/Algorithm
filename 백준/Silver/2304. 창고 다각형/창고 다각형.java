e84fa517c4f63c48af504e0e86c7a18386d2a3b8
YooKyuBong <kyubongbong@gmail.com>
2025-09-04 16:35:52 +0900

    [Silver I] Title: 창고 다각형, Time: 228 ms, Memory: 21272 KB -BaekjoonHub

---
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int tc = sc.nextInt();
+		
+		int[] walls = new int[1001];
+		int left = 1001;
+		int right = 0;
+		int maxH = 0;
+		int maxIdx = 0;
+		for(int t = 1; t <= tc; t++) {
+			int wall = sc.nextInt();
+			int height = sc.nextInt();
+	
+			walls[wall] = height;
+			
+			if(wall < left) left = wall;
+			if(wall > right) right = wall;
+			if(height > maxH) {
+				maxH = height;
+				maxIdx = wall;
+			}
+		}
+		
+		
+		
+		int sum = 0;
+		int currentMaxHeight = 0;
+		 
+		for(int i = left; i <= maxIdx; i++) {
+			
+			if(walls[i] > currentMaxHeight) currentMaxHeight = walls[i];
+			sum += currentMaxHeight;
+			
+			
+		}
+		
+		currentMaxHeight = 0;
+		for(int i = right; i > maxIdx; i--) {
+			if(walls[i] > currentMaxHeight) currentMaxHeight = walls[i];
+			sum += currentMaxHeight;
+		}
+		
+		
+
+		System.out.println(sum);
+		
+		
+		
+		
+	}
+
+}
