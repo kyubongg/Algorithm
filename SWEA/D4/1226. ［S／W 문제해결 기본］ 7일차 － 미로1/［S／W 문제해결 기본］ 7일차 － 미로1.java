9572f73a1b713d92bd41a98f3acf1ab96363fcad
YooKyuBong <kyubongbong@gmail.com>
2025-09-13 11:20:49 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 92 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -1,22 +1,20 @@
-
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution {
-	static int[][] drc = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-	static int N = 16;
-	static int[][] map;
-	static int finishR, finishC, ans;
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		for(int t = 1; t <= 10; t++) {
-			int tc = sc.nextInt();
+		for(int tc = 1; tc <= 10; tc++) {
+			int t = sc.nextInt();
 			
-			map = new int[N][N];
+			int N = 16;
+			
+			int[][] map = new int[N][N];
 			
 			int startR = 0, startC = 0;
-			finishR = 0;
-			finishC = 0;
 			
 			for(int i = 0; i < N; i++) {
 				String str = sc.next();
@@ -24,43 +22,64 @@ public class Solution {
 					int num = str.charAt(j) - '0';
 					map[i][j] = num;
 					
+					// 출발점 찾기
 					if(num == 2) {
 						startR = i;
 						startC = j;
 					}
-					if(num == 3) {
-						finishR = i;
-						finishC = j;
-					}
 				}
 			}
 			
-			ans = 0;
-			dfs(startR, startC);
+			// bfs 돌리기용 큐 만들기
+			Queue<int[]> q = new ArrayDeque<>();
+			
+			// 큐에 출발점 좌표 넣기
+			q.add(new int[] {startR, startC});
+			
+			// 사방탐색할때 상,하,좌,우 r,c 바꾸기
+			// 근데 문제에서 방향에 대한 입력이 존재하고, 방향이 1,2,3,4로 정해져 있으면
+			// 카운팅 배열처럼 0번 인덱스를 버려야하기 때문에 0번 인덱스에 0을 넣어주고
+			// 사방탐색할때 1~4까지 돌아야함(원래는 0~3까지 돔)
+//			int[] dr = {0, -1, 1, 0 ,0};
+//			int[] dc = {0, 0, 0, -1 ,1};
+			
+			int[] dr = {-1, 1, 0 ,0};
+			int[] dc = {0, 0, -1 ,1};
+			
+			// 방문처리용 배열
+			boolean[][] visited = new boolean[N][N];
+			int ans = 0;
+			// 큐가 빌때까지 반복 -> 출발점에서 도착점으로 갈 때까지 반복이랑 같은 의미
+			while(!q.isEmpty()) {
+				// 큐에서 한개 꺼냄(q를 선언할 때 int 배열로 선언해서
+				// 꺼낼때도 int 배열로 꺼내야함
+				int[] arr = q.poll();
+				
+				int r = arr[0];
+				int c = arr[1];
+				visited[r][c] = true;
+				
+				// 꺼냈을 때 좌표가 도착점이면 ans = 1(도착점에 도달가능)으로 해주고 break;
+				// 뒤에는 더이상 할 필요 없으니까 break 해주는 거임
+				if(map[r][c] == 3) {
+					ans = 1;
+					break;
+				}
+				// arr[0]: row값 들어있음, arr[1]: col값 들어잇음
+				for(int i = 0; i < 4; i++) {
+					int nr = r + dr[i];
+					int nc = c + dc[i];
+					
+					// 영역을 벗어나거나 이미 한번 들렸던곳이면 continue
+					if(nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
+					
+					// 사방탐색을 했을 때 갈 수 있는 길이면 큐에 넣고, 갈 수 없는 길이면 아무것도 안함
+					if(map[nr][nc] == 0 || map[nr][nc] == 3) q.add(new int[] {nr,nc});
+				}
+			}
 			
 			System.out.println("#" + t + " " + ans);
 		}
 	}
-	
-	static void dfs(int r, int c) {
-		if(ans == 1) return;
-		if(r == finishR && c == finishC) {
-			ans = 1;
-			return;
-		}
-				
-		map[r][c] = 1;
-		for(int k = 0; k < 4; k++) {
-			int nr = r + drc[k][0];
-			int nc = c + drc[k][1];
-			
-			if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
-			
-			if(map[nr][nc] == 0 || map[nr][nc] == 3) {
-//				System.out.println(nr+ " " +nc);
-				dfs(nr, nc);
-			}
-		}
 
-	}
 }
