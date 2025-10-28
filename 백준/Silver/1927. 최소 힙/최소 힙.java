8755109365a92ac6b037521f9bc1394d4b67ba6c
YooKyuBong <kyubongbong@gmail.com>
2025-10-28 17:25:00 +0900

    [Silver II] Title: 최소 힙, Time: 1456 ms, Memory: 118536 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for(int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+			if(num == 0) {
+				if(pq.isEmpty()) System.out.println(0);
+				else System.out.println(pq.poll());
+			}else {
+				pq.add(num);
+			}
+			
+		}
+		
+		sc.close();
+	}
+}
