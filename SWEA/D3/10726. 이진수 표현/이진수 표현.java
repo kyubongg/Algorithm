b875665efa14477f067bac3f13ad5f63512b20dd
YooKyuBong <kyubongbong@gmail.com>
2025-11-08 14:13:30 +0900

    [D3] Title: 이진수 표현, Time: 251 ms, Memory: 50,048 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -1,32 +1,30 @@
 import java.util.Scanner;
 
 public class Solution {
-
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int tc = sc.nextInt();
+		
 		for(int t = 1; t <= tc; t++) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
 			
-			String str = Integer.toBinaryString(M);
-			
-			boolean isOn = true;
-			
-			for(int i = str.length()-1; i >= str.length() - N; i--) {
-				
-				if(i < 0 || str.charAt(i) != '1') {
-					isOn = false;
+			boolean flag = true;
+			for(int i = 0; i < N; i++) {
+				if((M & (1<<i)) == 0) {
+					flag = false;
 					break;
 				}
-				
-				
 			}
-			
-			if(isOn) System.out.println("#" + t + " ON");
-			else System.out.println("#" + t + " OFF");
+			if(flag) {
+				System.out.println("#" + t + " ON");
+			}else {
+				System.out.println("#" + t + " OFF");
+			}
 		}
+		
+		sc.close();
 	}
-
 }
