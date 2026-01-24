dc60895681badbb855be467015c9be8438b6c31e
YooKyuBong <kyubongbong@gmail.com>
2026-01-25 00:17:51 +0900

    [Silver I] Title: 레슬러, Time: 316 ms, Memory: 21408 KB -BaekjoonHub

--- 백준/Silver/1900. 레슬러/레슬러.java
+++ 백준/Silver/1900. 레슬러/레슬러.java
@@ -1,7 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 	
@@ -27,7 +30,8 @@ public class Main {
 			return thisPower > InputPower ? 1 : -1;
 		}
 	}
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		
 		
 		// 경기력 공식 : ‘A의 힘’ + ‘B의 힘’ * ‘A가 착용하고 있는 마술 링의 힘’
@@ -36,24 +40,25 @@ public class Main {
 		// 나눠주는 금화의 수를 최소화함.
 		// Player들의 경기력 순서를 입력받는 동시에 계산함 (시간복잡도: N^2 -> N)
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int N = sc.nextInt();
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 		
 		List<Player> players = new ArrayList<>();
 		for(int i = 1; i <= N; i++) {
-			int power = sc.nextInt();
-			int ring = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
 			
-			players.add(new Player(i, power, ring));
+			players.add(new Player(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 			
 		}
 		
-        players.sort(null);
-        
+		players.sort(null);
+		
 		for(int i = players.size()-1; i >= 0; i--) {
 			System.out.println(players.get(i).idx);
 		}
 	}
-	
+
 }
