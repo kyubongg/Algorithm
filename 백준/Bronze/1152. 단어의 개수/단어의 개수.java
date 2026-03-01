f7192afb43bff7dc5dc5175825e0f33832bfa5e2
YooKyuBong <kyubongbong@gmail.com>
2026-03-01 22:28:38 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20040 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String line = br.readLine();
+        if (line == null) {
+            System.out.println(0);
+            return;
+        }
+
+        StringTokenizer st = new StringTokenizer(line, " ");
+        
+        System.out.println(st.countTokens());
+    }
+}
