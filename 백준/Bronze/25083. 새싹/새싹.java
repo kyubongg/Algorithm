9a434d530c1fcfb0e40cb7b7b1d696ee78928e24
YooKyuBong <kyubongbong@gmail.com>
2026-03-01 22:13:31 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 백슬래시(\)를 출력하려면 \\ 처럼 두 번 써야 합니다.
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |\n");
+        
+        System.out.print(sb.toString());
+    }
+}
