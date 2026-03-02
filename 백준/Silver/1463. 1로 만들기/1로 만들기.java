cf88221e5cad8f03b80d3af630803654c5950d3a
YooKyuBong <kyubongbong@gmail.com>
2026-03-02 10:56:45 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18216 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int zero;
+	static int one;
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int X = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[X+1];
+		
+		for (int i = 2; i <= X; i++) {
+			dp[i] = dp[i-1] +  1;
+			
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
+			}
+			
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
+			}
+		}
+		
+		System.out.println(dp[X]);
+	}
+	
+	
+}
