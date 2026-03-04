88c0577e2f38fa2248d077f1726294b9bf5aaaf3
YooKyuBong <kyubongbong@gmail.com>
2026-03-04 23:39:40 +0900

    [Silver II] Title: 최대 힙, Time: 792 ms, Memory: 33308 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if (x == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(pq.poll());
+				}
+			} else {
+				pq.add(x);
+			}
+		}
+		
+		
+	}
+	
+	
+}
