560709cf6a31e3d0e7ee0ce85e8cefe79a798c2f
YooKyuBong <kyubongbong@gmail.com>
2026-04-03 14:34:34 +0900

    [level 2] Title: 소수 찾기, Time: 26.89 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,45 +3,27 @@ import java.util.*;
 class Solution {
 
     static Set<Integer> answer;
-    static boolean[] visited;
+    
     public int solution(String numbers) {
         answer = new HashSet<>();
-        visited = new boolean[numbers.length()];
+    
+        StringBuilder sb = new StringBuilder();
+        getNum(sb, numbers, numbers.length(), -1);
         
-        backTrack("", numbers);
+        System.out.println(answer);
         
         return answer.size();
     }
     
-    static void backTrack(String curr, String numbers) {
-       
-        if (!curr.equals("")) {
-            int num = Integer.parseInt(curr);
-            
-            if (isPrime(num)) {
-                answer.add(num);
-            }
-        }
-                
-        for (int i = 0; i < numbers.length(); i++) {
-            if (visited[i]) continue;
-            
-            visited[i] = true;
-            backTrack(curr + numbers.charAt(i), numbers);
-            visited[i] = false;
+    static void getNum(StringBuilder sb, String numbers, int len, int prev) {
+        if (sb.length() == len) {
+           answer.add(Integer.parseInt(sb.toString()));
         }
         
-    }
-    
-    static boolean isPrime(int num) {
-        if (num < 2) return false;
-        
-        for (int i = 2; i <= (int)Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
+        for (int i = 0; i < len; i++) {
+            if(i == prev) continue;
+            sb.append(numbers.charAt(i));
+            getNum(sb, numbers, len, i);
         }
-        
-        return true;
     }
 }
