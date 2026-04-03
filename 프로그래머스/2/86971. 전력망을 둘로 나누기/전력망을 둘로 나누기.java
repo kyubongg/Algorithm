f7bcd412abab90f932f210fac13b585b3c5face9
YooKyuBong <kyubongbong@gmail.com>
2026-04-03 20:09:16 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.19 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,71 +1,71 @@
 import java.util.*;
 class Solution {
-    
-    static List<Integer>[] edges;
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
+        int answer = 101;
         
-        edges = new ArrayList[n+1];
-        
-        for(int i = 1; i <= n; i++) {
-            edges[i] = new ArrayList<>();
+        // 시간복잡도
+        // 간선 하나씩 제거 : n-1
+        // 제거한 후에 노드 연결된거 확인 : n-1
+        // O(N^2)
+        List<Integer>[] nodes = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = new ArrayList<>();
         }
         
-        
-        for(int i = 0; i < wires.length; i++) {
+        for (int[] wire : wires) {
+            int from = wire[0];
+            int to = wire[1];
             
-            int[] edge = wires[i];
-            
-            int from = edge[0];
-            int to = edge[1];
-            
-            // 무방향 그래프이므로 둘 다 추가
-            edges[from].add(to);
-            edges[to].add(from);
-            
+            nodes[from].add(to);
+            nodes[to].add(from);
         }
         
-        // for(int i = 1; i < edges.length; i++) {
-        //     System.out.println(edges[i]);
-        // }
-        
-        for(int i = 0; i < wires.length; i++) {
-            int[] nodes = wires[i];
+        // 간선 하나씩 제거
+        for (int[] wire : wires) {
+            int start = wire[0];
+            int end = wire[1];
             
-            int node1 = nodes[0];
-            int node2 = nodes[1];
+            int cnt = getNetworks(start, end, nodes);
             
-            int count = bfs(node1, node2, n);
+            int diff = Math.abs(cnt - (n - cnt));
             
-            answer = Math.min(answer, Math.abs(count - (n - count)));
+            answer = Math.min(answer, diff);
         }
+        
         
         return answer;
     }
     
-    static int bfs(int start, int skip, int n) {
+    
+    // 연결된거 확인
+    static int getNetworks(int start, int end, List<Integer>[] nodes) {
+        
         Queue<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[n+1];
+        boolean[] visited = new boolean[nodes.length];
         
         q.add(start);
         visited[start] = true;
+        int cnt = 1;
         
-        visited[skip] = true;
-        
-        int count = 1;
-        while(!q.isEmpty()) {
-            int node = q.poll();
+        while (!q.isEmpty()) {
             
-            for(int next : edges[node]) {
-                if(!visited[next]) {
+            int curr = q.poll();
+            
+            for (int next : nodes[curr]) {
+                
+                if (curr == start && next == end) continue;
+                
+                if (!visited[next]){
                     q.add(next);
                     visited[next] = true;
-                    count++;
+                    cnt++;                    
                 }
+                
             }
+            
         }
         
-        return count;
+        return cnt;
         
     }
 }
