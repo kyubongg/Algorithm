6721637619aea16508c80b017516b8545d0639d7
YooKyuBong <kyubongbong@gmail.com>
2026-04-04 21:28:43 +0900

    [Gold II] Title: 문제집, Time: 712 ms, Memory: 49264 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -1,56 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        List<Integer>[] exams = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            exams[i] = new ArrayList<>();
-        }
-
-        int[] indegrees = new int[N+1];
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-
-            exams[from].add(to);
-            indegrees[to]++;
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 1; i <= N; i++) {
-            if (indegrees[i] == 0) {
-                pq.add(i);
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            int exam = pq.poll();
-
-            sb.append(exam).append(" ");
-
-            for (int i = 0; i < exams[exam].size(); i++) {
-                indegrees[exams[exam].get(i)]--;
-                if (indegrees[exams[exam].get(i)] == 0) {
-                    pq.add(exams[exam].get(i));
-                }
-            }
-        }
-
-        System.out.println(sb.toString());
-    }
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		// 그래프 배열
+		List<Integer>[] exams = new ArrayList[N+1];
+		for (int i = 1; i <= N; i++) {
+			exams[i] = new ArrayList<>();
+		}
+		
+		// 진입차수 배열
+		int[] degrees = new int[N+1];
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			
+			exams[from].add(to);
+			degrees[to]++;
+		}
+		
+		
+		// bfs
+		PriorityQueue<Integer> q = new PriorityQueue<>();
+		for (int i = 1; i <= N; i++) {
+			if (degrees[i] == 0) {
+				q.add(i);
+			}
+		}
+		
+		List<Integer> answer = new ArrayList<>();
+		
+		while (!q.isEmpty()) {
+			int curr = q.poll();
+			
+			answer.add(curr);
+			for (int i = 0; i < exams[curr].size(); i++) {
+				int next = exams[curr].get(i);
+				
+				degrees[next]--;
+				if (degrees[next] == 0) {
+					q.add(next);
+				}
+			}
+		}
+		
+		for (int i = 0; i < answer.size(); i++) {
+			System.out.print(answer.get(i) + " ");
+		}
+		
+	}
+	
+	
 }
